95d4ec14f7840b9967baf346e6c45715647c31ce
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-04-14 13:29:26 -0600

    Primer commit

---
+++ src/main/java/com/nss/pibblest/PibblestApplication.java
@@ -0,0 +1,13 @@
+package com.nss.pibblest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibblestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibblestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nss/pibblest/PibblestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nss.pibblest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PibblestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
