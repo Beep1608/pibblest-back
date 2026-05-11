6f56635b98b58e044370230336d0ff3dd2869bca
Fidel Zarco <fidel.zarco1608@gmail.com>
2026-05-10 21:57:54 -0600

    Comencé modulo de ventas

---
+++ src/main/java/com/nss/pibblest/modules/sales/internal/infrastructure/data/SaleEntity.java
@@ -0,0 +1,10 @@
+package com.nss.pibblest.modules.sales.internal.infrastructure.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sales")
+public class SaleEntity {
+    
+}
